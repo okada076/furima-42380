f5300ad33e075f432c768bb40860b04ed5af289b
okada076 <416876pom@gmail.com>
2025-05-14 21:12:04 +0900

    商品価格入力のリアルタイム計算機能を追加

---
+++ app/javascript/item_price.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', () => {
+  const priceInput = document.getElementById("item-price");
+
+  if (priceInput) {
+    priceInput.addEventListener("input", () => {
+      const inputValue = priceInput.value;
+
+      const tax = Math.floor(inputValue * 0.1);
+      const profit = Math.floor(inputValue - tax);
+
+      const addTaxDom = document.getElementById("add-tax-price");
+      const profitDom = document.getElementById("profit");
+
+      if (addTaxDom && profitDom) {
+        addTaxDom.innerHTML = tax;
+        profitDom.innerHTML = profit;
+      }
+    });
+  }
+});
