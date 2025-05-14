93e54ec002834d611f259e04578de17c50b7746a
okada076 <416876pom@gmail.com>
2025-05-14 16:55:38 +0900

    コントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,27 @@ class ItemsController < ApplicationController
   def index
   end
 
+def new
+    @item = Item.new
+  end
 
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :name, :description, :category_id, :condition_id,
+      :shipping_fee_id, :prefecture_id, :scheduled_delivery_id,
+      :price, :image
+    )
+  end
 end
