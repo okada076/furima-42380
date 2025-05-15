4b864f1a5fab7cc6ac6663edf7637b374028f950
okada076 <416876pom@gmail.com>
2025-05-15 14:22:04 +0900

    商品出品機能　JavaScript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,4 +1,4 @@
-document.addEventListener('turbo:load', () => {
+const price = () => {
   const priceInput = document.getElementById("item-price");
 
  if (priceInput) {
@@ -17,4 +17,7 @@ document.addEventListener('turbo:load', () => {
       }
     });
   }
-});
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
