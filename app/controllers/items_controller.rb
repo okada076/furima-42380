4a4dbef15e1467c777314ccb18694227069f7bb0
okada076 <416876pom@gmail.com>
2025-05-17 19:55:40 +0900

    destroyアクションに未ログイン時のアクセス制限を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :redirect_unless_owner, only: [:edit, :update, :destroy]
 
   def index
