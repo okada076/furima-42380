e506ef71886bb1b9aa15588c6f6b8c0bcf4ddd3d
okada076 <416876pom@gmail.com>
2025-05-17 14:22:48 +0900

    編集・更新の共通処理をbefore_actionに整理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :redirect_unless_owner, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -22,15 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -49,7 +47,10 @@ class ItemsController < ApplicationController
   end
 
   def redirect_unless_owner
-    @item = Item.find(params[:id])
     redirect_to root_path if current_user != @item.user
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
