3cde6f7ac480987ab2859ea0ee18c7c574c6c7fe
okada076 <416876pom@gmail.com>
2025-05-21 20:31:44 +0900

    Rubocopによるコードスタイル修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,10 @@
 class Order < ApplicationRecord
   attr_accessor :token
+
   validates :token, presence: true
   validates :user_id, presence: true
   validates :item_id, presence: true
   belongs_to :user
   belongs_to :item
   has_one :address
-
-
 end
