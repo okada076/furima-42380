3f816d0567ad4113f9a742c46f811b10a4a7c4f4
okada076 <416876pom@gmail.com>
2025-05-21 17:21:03 +0900

    サーバーサイドで環境変数を読み込めるように修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,7 @@ class OrdersController < ApplicationController
   before_action :set_item
 
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @order_address = OrderAddress.new
   end
 
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,6 +1,7 @@
 const pay = () => {
- const payjp = Payjp('pk_test_7620aaafc345a40a562f9baf')// PAY.JPテスト公開鍵
-const elements = payjp.elements();
+  const publicKey = gon.public_key
+  const payjp = Payjp(publicKey) // PAY.JPテスト公開鍵
+  const elements = payjp.elements();
   const numberElement = elements.create('cardNumber');
   const expiryElement = elements.create('cardExpiry');
   const cvcElement = elements.create('cardCvc');
