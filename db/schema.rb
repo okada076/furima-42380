fa3d9fd7ab0b5b09a1e464265adc81a690ddfdf3
okada076 <416876pom@gmail.com>
2025-05-21 21:54:25 +0900

    orders・addressesテーブルに外部キー制約とnull制約を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_21_031604) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_21_125102) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,13 +42,14 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_21_031604) do
   create_table "addresses", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "postal_code"
-    t.integer "prefecture_id"
-    t.string "city"
-    t.string "address"
+    t.string "postal_code", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", null: false
+    t.string "address", null: false
     t.string "building_name"
-    t.string "phone_number"
-    t.integer "order_id"
+    t.string "phone_number", null: false
+    t.bigint "order_id"
+    t.index ["order_id"], name: "index_addresses_on_order_id"
   end
 
   create_table "items", charset: "utf8mb3", force: :cascade do |t|
@@ -69,8 +70,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_21_031604) do
   create_table "orders", charset: "utf8mb3", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
-    t.integer "item_id"
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -93,5 +96,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_21_031604) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "orders"
   add_foreign_key "items", "users"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
