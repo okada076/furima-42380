388dc57b41d142695098d78217b74e482c1041e4
okada076 <416876pom@gmail.com>
2025-05-21 16:58:23 +0900

    購入情報のバリデーションを整理し、重複エラーメッセージを解消

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -5,12 +5,16 @@ class OrderAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :postal_code
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
+    validates :phone_number
     validates :token
   end
+  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }, if: lambda {
+    postal_code.present?
+  }
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }, if: -> { phone_number.present? }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
